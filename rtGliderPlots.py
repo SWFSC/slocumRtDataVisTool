d89ebb51703d392821a8183d60fa8ab5c598def4
Caleb Flaim <caleb.flaim@noaa.gov>
2024-11-15 21:15:29 +0000

    rm pw

--- rtGliderPlots.py
+++ rtGliderPlots.py
@@ -67,7 +67,6 @@ class gliderData:
         self.glider = self.deployment.split("-")[0]
         self.project = args.project
         self.max_amphrs = args.amphours
-        print(self.max_amphrs)
         self.new_data_bool = False
 
         # Data path info
@@ -828,7 +827,7 @@ class doEmail:
         self.recipiants = ["caleb.flaim@noaa.gov", "esdgliders@gmail.com"] #nmfs.swfsc.esd-gliders@noaa.gov , "jacob.partida@noaa.gov", 
                         #    "jen.walsh@noaa.gov", "anthony.cossio@noaa.gov", "christian.reiss@noaa.gov",
                         #    "eric.bjorkstedt@noaa.gov"
-        self.password =  "dyzw kqlu daop oemy" # to fill in on VM  # access_secret_version('ggn-nmfs-usamlr-dev-7b99', 'esdgliders-email')input("Type your password and press enter:")
+        self.password = # to fill in on VM  # access_secret_version('ggn-nmfs-usamlr-dev-7b99', 'esdgliders-email')input("Type your password and press enter:")
     
     def sendNoData(self):
         message = MIMEMultipart()
